eab7d104946d5ab5444f0e2705dc78ba32bfff72
Emeka Chukwulobe <emekabe@yahoo.com>
2020-04-05 17:19:49 +0100

    Started Working on the main activity

--- app/src/main/java/com/emekachukwulobe/sees19yearbookdemo/MainActivity.java
+++ app/src/main/java/com/emekachukwulobe/sees19yearbookdemo/MainActivity.java
@@ -8,20 +8,11 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myTextView = findViewById(R.id.my_textview);
-
-        myTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                myTextView.setText("Hello Emeka!");
-            }
-        });
     }
 }
