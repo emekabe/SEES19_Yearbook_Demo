c8acf117a4981b3d97093249cc74985791c635c0
Emeka Chukwulobe <emekabe@yahoo.com>
2020-04-09 11:57:37 +0100

    Splash screen

---
+++ app/src/main/java/com/emekachukwulobe/sees19yearbookdemo/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.emekachukwulobe.sees19yearbookdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+    }
+}
