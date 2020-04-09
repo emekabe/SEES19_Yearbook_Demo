5e03d19cc0adafb21ccbe0f5a26f24c82fa536d2
Emeka Chukwulobe <emekabe@yahoo.com>
2020-04-09 14:26:39 +0100

    AboutActivity done

--- app/src/main/java/com/emekachukwulobe/sees19yearbookdemo/AboutActivity.java
+++ app/src/main/java/com/emekachukwulobe/sees19yearbookdemo/AboutActivity.java
@@ -1,5 +1,6 @@
 package com.emekachukwulobe.sees19yearbookdemo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,8 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
